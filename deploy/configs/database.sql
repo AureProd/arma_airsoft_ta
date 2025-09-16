18580a3520b7ed6e55a344afe8ad060c5e9f256c
aureprod <aureprod0@gmail.com>
2025-09-16 16:15:15 +0200

    ci: fix ci + fix db migration file + fix server config

--- deploy/configs/database.sql
+++ deploy/configs/database.sql
@@ -3,16 +3,18 @@ DROP TABLE IF EXISTS player;
 
 CREATE TABLE player
 (
-   p_id INTEGER AUTO_INCREMENT NOT NULL,
-   p_uid VARCHAR(100) NOT NULL,
-   p_name VARCHAR(100) NOT NULL,
-   p_money INTEGER(100) NOT NULL,
-   p_tenues TEXT NOT NULL,
-   p_kills INTEGER(100) NOT NULL,
-   p_morts INTEGER(100) NOT NULL,
-   p_win_games INTEGER(100) NOT NULL,
-   p_games_played INTEGER(100) NOT NULL,
-   p_niv_vip INTEGER(10) NOT NULL,
-   p_niv_droits INTEGER(10) NOT NULL,
-   PRIMARY KEY (p_id)
+   id INTEGER AUTO_INCREMENT NOT NULL,
+   uid VARCHAR(100) NOT NULL,
+   name VARCHAR(100) NOT NULL,
+   money INTEGER(100) NOT NULL,
+   blue_outfits TEXT NOT NULL,
+   red_outfits TEXT NOT NULL,
+   kills INTEGER(100) NOT NULL,
+   deads INTEGER(100) NOT NULL,
+   won_games INTEGER(100) NOT NULL,
+   lost_games INTEGER(100) NOT NULL,
+   vip_level INTEGER(10) NOT NULL,
+   rights INTEGER(10) NOT NULL,
+   PRIMARY KEY (id),
+   UNIQUE KEY uniq_uid (uid)
 ) ENGINE=InnoDB CHARSET=utf8;
